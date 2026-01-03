8021f059ab3dd505032830138584f03bcaf3aa89
Abchoudhary2512 <bhakarajay033@gmail.com>
2026-01-03 21:07:31 +0530

    fixed the ondelete

--- components/HabitList.tsx
+++ components/HabitList.tsx
@@ -1,13 +1,13 @@
 import HabitCard from "./HabitCard";
 
-export default function HabitList({ habits, onToggle }: any) {
+export default function HabitList({ habits, onToggle, onDelete }: any) {
   if (!habits.length)
     return <p className="text-gray-400">Add your first habit</p>;
 
   return (
     <div className="space-y-3">
       {habits.map((h: any) => (
-        <HabitCard key={h.id} habit={h} onToggle={onToggle} />
+        <HabitCard key={h.id} habit={h} onToggle={onToggle} onDelete={onDelete} />
       ))}
     </div>
   );
